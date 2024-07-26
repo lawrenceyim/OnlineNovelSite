3a6a6f358f847987becca55cd2108eea375a0bae
Lawrence Y <84044148+lawrenceyim@users.noreply.github.com>
2024-07-26 18:00:55 -0400

    Added CSS for body paragraphs.

--- react/src/components/Chapter.tsx
+++ react/src/components/Chapter.tsx
@@ -13,11 +13,12 @@ export interface ChapterProp {
 
 export default function Chapter(prop: ChapterProp) {
     const [currentChapter, setCurrentChapter] = useState<number>(prop.chapterId)
+    const paragraphs: string[] = prop.body.split("\n");
     return <div className="chapterPage">
         <h1>{prop.bookTitle}</h1>
         <ChapterNavBar bookId={prop.bookId} currentChapterId={currentChapter} chapterList={prop.chapterList} setChapter={setCurrentChapter}/>
         <h2>{prop.chapterTitle}</h2>
-        <p>{prop.body}</p>
+        {paragraphs.map(paragraph => <p>{paragraph}</p>)}
         <ChapterNavBar bookId={prop.bookId} currentChapterId={currentChapter} chapterList={prop.chapterList} setChapter={setCurrentChapter}/>
     </div>;
 }
